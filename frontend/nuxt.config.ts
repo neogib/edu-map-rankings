231c117fea317866a64991cd5be6ca2cfa1c6293
Gibirizon <swojteksz@gmail.com>
2025-12-25 20:31:32 +0100

    feat: add tailwind and maplibre nuxt module

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,17 @@
+import tailwindcss from "@tailwindcss/vite"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint", "@nuxt/ui"],
+    modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-maplibre"],
     // Runtime environment configuration
     runtimeConfig: {
         public: {
             apiBase: "http://localhost:8000",
         },
+    },
+    css: ["./app/assets/css/main.css"],
+    vite: {
+        plugins: [tailwindcss()],
     },
 })
