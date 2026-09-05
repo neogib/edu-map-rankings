07828ccf8763daf21fd7ee6bc6ac857f45c2ab96
neogib <swojteksz@gmail.com>
2026-09-05 21:32:36 +0200

    feat: add cors middleware for capacitor

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.exception_handlers import register_exception_handlers
 from app.api.v1.router import api_v1_router
@@ -7,6 +8,23 @@ from app.core.logging import configure_logging
 configure_logging()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://localhost",
+    "capacitor://localhost",
+    "https://eduradar.4one.ovh",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 register_exception_handlers(app)
 app.include_router(api_v1_router, prefix="/api/v1")
 
